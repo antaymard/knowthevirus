bc2990b3599959d4d0f4b5c696726d1eb259b367
antaymard <antaymard@gmail.com>
2020-03-25 19:19:33 +0100

    update gitignore and clean code structure

---
+++ dataFetching.js
@@ -0,0 +1,24 @@
+const GitFileDownloader = require('git-file-downloader');
+
+// Passer en série de promises TODO
+
+// Dowload the csv file from Github
+var downloader = new GitFileDownloader({
+    provider: 'github',
+    repository: 'CSSEGISandData/COVID-19',
+    branch: 'master',
+    file: 'csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv',
+    output: './data'
+});
+
+downloader.run()
+    .then(() => {
+        console.log("File downloaded from Github".success)
+    })
+    .catch(err => {
+        console.log('Error downloading data from Github'.error)
+        console.error(err);
+        // process.exitCode = 1;
+    });
+
+// ======= END OF FIle 
