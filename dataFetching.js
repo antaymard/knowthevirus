d737a5f0c1acffc195134d9f6d54c07c17e65469
antaymard <antaymard@gmail.com>
2020-03-30 20:29:11 +0200

    fix url for downloads

--- dataFetching.js
+++ dataFetching.js
@@ -6,7 +6,7 @@ var schedule = require('node-schedule');
 let dowloads = [
     "csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv",
     "csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv",
-    "csse_covid_19_datva/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+    "csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
 ];
 
 let downloadPromises = [];
