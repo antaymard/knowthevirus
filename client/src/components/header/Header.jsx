62a6298931154814cd7059fc0a40929668dcc98b
antaymard <antaymard@gmail.com>
2020-04-16 01:06:16 +0200

    fix header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./header.css";
+import { Link } from "react-router-dom";
 //import img from "../../image/virus.jpg";
 
 function Header() {
   return (
     <header>
       <div className="header-section">
+        <Link to="/">KNOWTHEVIRUS</Link>
         {/* <img className="logo" src={img} alt="" /> */}
         {/* <input id="country-searchbar" placeholder="Rechercher un pays"></input> */}
         <div />
